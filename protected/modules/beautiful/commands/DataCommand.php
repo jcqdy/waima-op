fe66a9f070232f1b6a73a3ddcdb9769c43a49556
jcqdy <jcqdy@sina.com>
2018-03-27 16:31:48 +0800

    no message

--- protected/modules/beautiful/commands/DataCommand.php
+++ protected/modules/beautiful/commands/DataCommand.php
@@ -4,7 +4,7 @@ class DataCommand extends ConsoleCommand
 {
     public function actionA()
     {
-        $n = 10;
+        $n = 100;
         $modelDaoPlayer = new ModelDaoPlayer();
 
         while ($n > 0) {
@@ -13,7 +13,8 @@ class DataCommand extends ConsoleCommand
                 'company' => '万达影业',
                 'job' => '编剧',
                 'picUrl' => 'timg.jpeg',
-                'uploadTime' => rand(0, 99),
+                'like' => rand(0, 199),
+                'uploadTime' => time(),
                 'contact' => '15951960273',
             ];
 
@@ -21,17 +22,17 @@ class DataCommand extends ConsoleCommand
             $n--;
         }
 
-        $modelDaoMessage = new ModelDaoMessage();
-        $m = 10;
-
-        while ($m > 0) {
-            $dat = [
-                'content' => '我的评论,时尚最时尚,在光滑的屏幕上摩擦,摩擦,在皎洁的月光下摩擦,摩擦',
-            ];
-            $dat['createTime'] = time() - rand(0, 86400);
-
-            $modelDaoMessage->add($dat);
-            $m--;
-        }
+//        $modelDaoMessage = new ModelDaoMessage();
+//        $m = 10;
+//
+//        while ($m > 0) {
+//            $dat = [
+//                'content' => '我的评论,时尚最时尚,在光滑的屏幕上摩擦,摩擦,在皎洁的月光下摩擦,摩擦',
+//            ];
+//            $dat['createTime'] = time() - rand(0, 86400);
+//
+//            $modelDaoMessage->add($dat);
+//            $m--;
+//        }
     }
 }
