b05ed2fbf90cfaaf69556939446a28df9640d1d8
jcqdy <jcqdy@sina.com>
2018-04-02 14:55:16 +0800

    no message

--- protected/modules/beautiful/controllers/SearchController.php
+++ protected/modules/beautiful/controllers/SearchController.php
@@ -4,6 +4,8 @@ class SearchController extends H5Controller
 {
     public function actionIndex()
     {
+        LogHelper::error($_SERVER['HTTP_USER_AGENT']);
+
         $keyword = ParameterValidatorHelper::validateArray($_REQUEST, 'keyword', ' ');
         $sp = ParameterValidatorHelper::validateInteger($_REQUEST, 'sp', 0, PHP_INT_MAX, 0);
         $num = ParameterValidatorHelper::validateInteger($_REQUEST, 'num', 1, 100, 30);
